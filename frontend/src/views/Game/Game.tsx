cf2c01efcc64b610486d6316bb3768153916ca34
ked00 <treemansquad@gmail.com>
2023-03-13 07:19:29 -0500

    text to speech added

--- frontend/src/views/Game/Game.tsx
+++ frontend/src/views/Game/Game.tsx
@@ -1,6 +1,7 @@
 import {Container} from "@mui/material";
 import {useEffect} from "react";
 import {useSpeechRecognition} from "react-speech-recognition";
+import useSpeech from "use-speech18";
 
 // components
 import {BlockButton} from "../../components/Buttons/BlockButton";
@@ -18,7 +19,14 @@ import {useLoopArray} from "../../reuseable-hooks/loopArray";
 import {useVisible} from "../../reuseable-hooks/visible";
 
 type Props = {
-  getInfo: {info: {time: number; sentence: number; questions:{main:[{question: string}], translated:[{question: string}] }}; gameInfo: () => void};
+  getInfo: {
+    info: {
+      time: number;
+      sentence: number;
+      questions: {main: [{question: string}]; translated: [{question: string}]};
+    };
+    gameInfo: () => void;
+  };
   points: {
     correct: number;
     wrong: number;
@@ -34,6 +42,7 @@ export function Game(props: Props) {
   const toggle = useVisible(false);
   const switchPage = useLoopArray(0, props.getInfo.info.sentence);
   const {finalTranscript} = useSpeechRecognition();
+  const {Speak, speak_utils} = useSpeech();
 
   const handleUp = () => {
     handleMouseUp();
@@ -42,11 +51,17 @@ export function Game(props: Props) {
 
   useEffect(() => {
     props.getInfo.gameInfo();
+    speak_utils.setVoice(speak_utils.Voices[0].name);
+    speak_utils.setPitch(2);
+    speak_utils.setRate(0.8);
   }, []);
 
   const next = () => {
     switchPage.check(chances.chancesLeft);
-    if (finalTranscript.toLowerCase() === props.getInfo.info.questions.main[switchPage.currentIndex].question) {
+    if (
+      finalTranscript.toLowerCase() ===
+      props.getInfo.info.questions.main[switchPage.currentIndex].question
+    ) {
       switchPage.nextIndex();
       toggle.strict(false);
       props.points.increase();
@@ -71,8 +86,12 @@ export function Game(props: Props) {
           props.getInfo.info.sentence
         }`}</h1>
         <Container>
-          <LearningLangugaeQuestion text={props.getInfo.info.questions.main[switchPage.currentIndex].question} />
-          <NativeLanguageTranslation text={props.getInfo.info.questions.translated[switchPage.currentIndex].question} />
+          <LearningLangugaeQuestion
+            text={props.getInfo.info.questions.main[switchPage.currentIndex].question}
+          />
+          <NativeLanguageTranslation
+            text={props.getInfo.info.questions.translated[switchPage.currentIndex].question}
+          />
           <div className="text-center mt-5">
             {/* <img
               src={questions[switchPage.currentIndex].img}
@@ -81,7 +100,15 @@ export function Game(props: Props) {
               height="600px"
             /> */}
           </div>
-          <BlockButton type="outlined" text="Listen" className="w-100 mt-5 text-dark" />
+          <BlockButton
+            type="outlined"
+            text="Listen"
+            className="w-100 mt-5 text-dark"
+            onClick={() => {
+              Speak(props.getInfo.info.questions.main[switchPage.currentIndex].question);
+            }}
+          />
+
           <BlockButton
             type="contained"
             text={!toggle.isVisible ? "Speak" : "Stop Speaking"}
